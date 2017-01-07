67c00160e125ab431b0c9eb2e0157ee76a0f4e3e
lgraesser <lhgraesser@gmail.com>
2017-01-07 18:52:01 -0500

    Adding more import tests

--- test/test_install.py
+++ test/test_install.py
@@ -5,6 +5,16 @@ class InstallTest():
         import keras
         assert('keras' in sys.modules)
 
+    def matplotlib_import(self):
+        import sys
+        import matplotlib
+        assert('matplotlib' in sys.modules)
+
+    def numpy_import(self):
+        import sys
+        import numpy
+        assert('numpy' in sys.modules)
+
     def simple_nn(self):
         import numpy as np
         from keras.models import Sequential
@@ -26,5 +36,7 @@ class InstallTest():
 if __name__ == "__main__":
     test = InstallTest()
     test.keras_import()
+    test.matplotlib_import()
+    test.numpy_import()
     test.simple_nn()
     print("If no assertion errors have been printed above, all tests passed and you are good to go!")
